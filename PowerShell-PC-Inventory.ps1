f3ce1c724bef3dd95f0c08070b6bec9716157763
sbirdsill <43529733+sbirdsill@users.noreply.github.com>
2019-07-04 17:34:19 -0700

    Update PowerShell-PC-Inventory.ps1

--- PowerShell-PC-Inventory.ps1
+++ PowerShell-PC-Inventory.ps1
@@ -1,22 +1,31 @@
-# PowerShell CSV PC Inventory Login Script
+# PowerShell CSV PC Inventory Script
 # PowerShell-PC-Inventory.ps1
-# Version 1.0
-# Last updated: 11/26/2018
+# Version 1.1
+# Last updated: Jul-3-2019
 #
-# This PowerShell script will collect the Date, IP, Serial Number, Model, CPU, RAM, Storage, OS and OS Build of a computer. It will then output it into a CSV file.
-# Before it does that, it will check the CSV file to see if the hostname is already in the file. If it is, it will exit the script. Otherwise, it will log the information.
-# It is designed to be run as a login script/immediate task run by a domain user. Elevated privileges are not required.
+# This PowerShell script will collect the Date of inventory, IP and MAC address, serial number, model, CPU, RAM, total storage size, GPU(s), OS, OS build, logged in user, and the attached monitor(s) of a computer.
+# After it collects that information, it is outputted to a CSV file. It will first check the CSV file (if it exists) to see if the hostname already exists in the file. 
+# If hostname exists in the CSV file, it will overwrite it with the latest information so that the inventory is up to date and there is no duplicate information.
+# It is designed to be run as a login script and/or a scheduled/immediate task run by a domain user. Elevated privileges are not required.
 #
-# IMPORTANT: Parts that will need be modified for your environment are double commented (##). The rest can safely be left alone.
+# IMPORTANT: Parts that may need be modified for your environment are double commented (##). The rest of the script can safely be left as is.
 
-## CSV File Location (remember to allow Write permissions for the "Everyone" or "Domain Users" group)
+## CSV File Location (If this doesn't exist, the script will attempt to create it. Users will need full control of the file.)
 $csv = "$pwd\Inventory.csv"
 
+## Error log path (Optional but recommended. If this doesn't exist, the script will attempt to create it. Users will need full control of the file.)
+$ErrorLogPath = "$pwd\PowerShell-PC-Inventory-Error-Log.log"
+
+Write-Host "Gathering inventory information..."
+
 # Date
-$Date = Get-Date -Format yyyy-MM-dd
+$Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
 # IP
 $IP = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.DefaultIPGateway -ne $null }).IPAddress | Select-Object -First 1
+
+# MAC address
+$MAC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.DefaultIPGateway -ne $null } | Select-Object -ExpandProperty MACAddress
 
 # Serial Number
 $SN = Get-WmiObject -Class Win32_Bios | Select-Object -ExpandProperty SerialNumber
@@ -31,7 +40,19 @@ $CPU = Get-WmiObject -Class win32_processor | Select-Object -ExpandProperty Name
 $RAM = Get-WmiObject -Class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object { [math]::Round(($_.sum / 1GB),2) }
 
 # Storage
-$Storage = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'" | ForEach-Object { [math]::Round($_.Size / 1GB,2) }
+$Storage = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='$env:systemdrive'" | ForEach-Object { [math]::Round($_.Size / 1GB,2) }
+
+#GPU(s)
+function GetGPUInfo {
+  $GPUs = Get-WmiObject -Class Win32_VideoController
+  foreach ($GPU in $GPUs) {
+    $GPU | Select-Object -ExpandProperty Description
+  }
+}
+
+## If some computers have more than two GPUs, you can copy the lines below, but change the variable and index number by counting them up by 1.
+$GPU0 = GetGPUInfo | Select-Object -Index 0
+$GPU1 = GetGPUInfo | Select-Object -Index 1
 
 # OS
 $OS = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty Caption
@@ -39,46 +60,204 @@ $OS = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty
 # OS Build
 $OSBuild = (Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')
 
+# Username
+$Username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+# Monitor(s)
+function GetMonitorInfo {
+  # Thanks to https://github.com/MaxAnderson95/Get-Monitor-Information
+  $Monitors = Get-WmiObject -Namespace "root\WMI" -Class "WMIMonitorID"
+  foreach ($Monitor in $Monitors) {
+    ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)","")
+    ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)","")
+    ([System.Text.Encoding]::ASCII.GetString($Monitor.SerialNumberID)).Replace("$([char]0x0000)","")
+  }
+}
+
+## If some computers have more than three monitors, you can copy the lines below, but change the variable and index number by counting them up by 1.
+$Monitor1 = GetMonitorInfo | Select-Object -Index 0,1
+$Monitor1SN = GetMonitorInfo | Select-Object -Index 2
+$Monitor2 = GetMonitorInfo | Select-Object -Index 3,4
+$Monitor2SN = GetMonitorInfo | Select-Object -Index 5
+$Monitor3 = GetMonitorInfo | Select-Object -Index 6,7
+$Monitor3SN = GetMonitorInfo | Select-Object -Index 8
+
+$Monitor1 = $Monitor1 -join ' '
+$Monitor2 = $Monitor2 -join ' '
+$Monitor2 = $Monitor2 -join ' '
+
+# Type of computer
+$Chassis = Get-CimInstance -ClassName Win32_SystemEnclosure -Namespace 'root\CIMV2' -Property ChassisTypes | Select-Object -ExpandProperty ChassisTypes
+# Values are from https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-systemenclosure
+if ($Chassis -eq "1") {
+  $Chassis = "Other"
+}
+if ($Chassis -eq "2") {
+  $Chassis = "Unknown"
+}
+if ($Chassis -eq "3") {
+  $Chassis = "Desktop"
+}
+if ($Chassis -eq "4") {
+  $Chassis = "Low Profile Desktop"
+}
+if ($Chassis -eq "5") {
+  $Chassis = "Pizza Box"
+}
+if ($Chassis -eq "6") {
+  $Chassis = "Mini Tower"
+}
+if ($Chassis -eq "7") {
+  $Chassis = "Tower"
+}
+if ($Chassis -eq "8") {
+  $Chassis = "Portable"
+}
+if ($Chassis -eq "9") {
+  $Chassis = "Laptop"
+}
+if ($Chassis -eq "10") {
+  $Chassis = "Notebook"
+}
+if ($Chassis -eq "11") {
+  $Chassis = "Hand Held"
+}
+if ($Chassis -eq "12") {
+  $Chassis = "Docking Station"
+}
+if ($Chassis -eq "13") {
+  $Chassis = "All in One"
+}
+if ($Chassis -eq "14") {
+  $Chassis = "Sub Notebook"
+}
+if ($Chassis -eq "15") {
+  $Chassis = "Space-Saving"
+}
+if ($Chassis -eq "16") {
+  $Chassis = "Lunch Box"
+}
+if ($Chassis -eq "17") {
+  $Chassis = "Main System Chassis"
+}
+if ($Chassis -eq "18") {
+  $Chassis = "Expansion Chassis"
+}
+if ($Chassis -eq "19") {
+  $Chassis = "SubChassis"
+}
+if ($Chassis -eq "20") {
+  $Chassis = "Bus Expansion Chassis"
+}
+if ($Chassis -eq "21") {
+  $Chassis = "Peripheral Chassis"
+}
+if ($Chassis -eq "22") {
+  $Chassis = "Storage Chassis"
+}
+if ($Chassis -eq "23") {
+  $Chassis = "Rack Mount Chassis"
+}
+if ($Chassis -eq "24") {
+  $Chassis = "Sealed-Case PC"
+}
+
 # Function to write the inventory to the CSV file
-function inventory {
+function OutputToCSV {
   # CSV properties
-  # https://gallery.technet.microsoft.com/scriptcenter/PowerShell-Script-Get-beced710
+  # Thanks to https://gallery.technet.microsoft.com/scriptcenter/PowerShell-Script-Get-beced710
+  Write-Host "Adding inventory information to the CSV file..."
   $infoObject = New-Object PSObject
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Date Collected" -Value $Date
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "IP" -Value $IP
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "IP Address" -Value $IP
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Hostname" -Value $env:computername
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "User" -Value $env:UserName
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "MAC Address" -Value $MAC
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "User" -Value $Username
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Type" -Value $Chassis
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Serial Number/Service Tag" -Value $SN
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Model" -Value $Model
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "CPU" -Value $CPU
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "RAM (GB)" -Value $RAM
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Storage (GB)" -Value $Storage
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "GPU 0" -Value $GPU0
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "GPU 1" -Value $GPU1
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS" -Value $OS
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS Version" -Value $OSBuild
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 1" -Value $Monitor1
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 1 Serial Number" -Value $Monitor1SN
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 2" -Value $Monitor2
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 2 Serial Number" -Value $Monitor2SN
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 3" -Value $Monitor3
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 3 Serial Number" -Value $Monitor3SN
   $infoObject
   $infoColl += $infoObject
 
   # Output to CSV file
-  $infoColl | Export-Csv -Path $csv -NoTypeInformation -Append
+  try {
+    $infoColl | Export-Csv -Path $csv -NoTypeInformation -Append
+    Write-Host -ForegroundColor Green "Inventory was successfully updated!"
+    # Clean up empty rows
+    (Get-Content $csv) -notlike ",,,,,,,,,,,,,,,,,,,,*" | Set-Content $csv
+    exit 0
+  }
+  catch {
+    if (-not (Test-Path $ErrorLogPath))
+    {
+      New-Item -ItemType "file" -Path $ErrorLogPath
+      icacls $ErrorLogPath /grant Everyone:F
+    }
+    Add-Content -Path $ErrorLogPath -Value "[$Date] $Username at $env:computername was unable to export to the inventory file at $csv."
+    throw "Unable to export to the CSV file. Please check the permissions on the file."
+    exit 1
+  }
 }
 
-# Just in case the inventory CSV file doesn't exist, go ahead and create the file and run the inventory.
+# Just in case the inventory CSV file doesn't exist, create the file and run the inventory.
 if (-not (Test-Path $csv))
 {
-  inventory
+  Write-Host "Creating CSV file..."
+  try {
+    New-Item -ItemType "file" -Path $csv
+    icacls $csv /grant Everyone:F
+    OutputToCSV
+  }
+  catch {
+    if (-not (Test-Path $ErrorLogPath))
+    {
+      New-Item -ItemType "file" -Path $ErrorLogPath
+      icacls $ErrorLogPath /grant Everyone:F
+    }
+    Add-Content -Path $ErrorLogPath -Value "[$Date] $Username at $env:computername was unable to create the inventory file at $csv."
+    throw "Unable to create the CSV file. Please check the permissions on the file."
+    exit 1
+  }
 }
 
-# First, verify and check to see if the S/N of the machine is already in the inventory. If it is, exit the script, othwerise run the inventory.
-function Verify {
+# Check to see if the CSV file exists then run the script.
+function Check-IfCSVExists {
+  Write-Host "Checking to see if the CSV file exists..."
   $import = Import-Csv $csv
   if ($import -match $env:computername)
   {
-    break
+    try {
+      (Get-Content $csv) -notmatch $env:computername | Set-Content $csv
+      OutputToCSV
+    }
+    catch {
+      if (-not (Test-Path $ErrorLogPath))
+      {
+        New-Item -ItemType "file" -Path $ErrorLogPath
+        icacls $ErrorLogPath /grant Everyone:F
+      }
+      Add-Content -Path $ErrorLogPath -Value "[$Date] $Username at $env:computername was unable to import and/or modify the inventory file located at $csv."
+      throw "Unable to import and/or modify the CSV file. Please check the permissions on the file."
+      exit 1
+    }
   }
   else
   {
-    inventory
+    OutputToCSV
   }
 }
 
-Verify
+Check-IfCSVExists
