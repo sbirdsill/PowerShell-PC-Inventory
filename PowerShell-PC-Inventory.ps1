5a31c7b36d4d181989a5a4248900ee913ecb790a
sbirdsill <43529733+sbirdsill@users.noreply.github.com>
2024-11-14 20:29:06 -0800

    Update PowerShell-PC-Inventory.ps1
    - Replaced Get-WmiObject cmdlet with Get-CimInstance
    - Updated comments
    - Updated default output variables from $pwd to c:\scripts (make sure to change this)
    - Added ability to gather a list of installed apps, BIOS info, and up time
    - Script will no longer check the existing CSV file for previous entries (this was a holdover from a previous release)
    - Rewrote the portion that grabs the IP and MAC address of the computer to get the NIC that routes traffic to 0.0.0.0/0 with the lowest priority 
    - Fixed the issue where the OS version of the OS was incorrect (it was relevant to Windows 10 but does not appear to output correctly on Windows 11). Adjusted it to output the build number instead.

--- PowerShell-PC-Inventory.ps1
+++ PowerShell-PC-Inventory.ps1
@@ -4,22 +4,31 @@ param(
 
 # PowerShell CSV PC Inventory Script
 # PowerShell-PC-Inventory.ps1
-# Version 1.2
-# Last updated: Sep-4-2023
+# Version 1.3
+# Last updated: Nov-14-2024
 #
-# This PowerShell script will collect the Date of inventory, IP and MAC address, serial number, model, CPU, RAM, total storage size, GPU(s), OS, OS build, logged in user, and the attached monitor(s) of a computer.
-# After it collects that information, it is outputted to a CSV file. It will first check the CSV file (if it exists) to see if the hostname already exists in the file. 
-# If hostname exists in the CSV file, it will overwrite it with the latest information so that the inventory is up to date and there is no duplicate information.
+# This PowerShell script will collect the Date of inventory, host name, IP/MAC, username (run as user), type, serial number, model, BIOS info, CPU, RAM, storage (OS drive), GPU(s), OS and version, up time, monitor(s), and installed apps (optional) on a computer. 
+# After it collects that information, it is outputted to a CSV file.
+# 
 # It is designed to be run as a login script and/or a scheduled/immediate task run by a domain user. Elevated privileges are not required.
+# Each inventory file can be complied into one CSV file by running the -c parameter.
 #
-# IMPORTANT: Parts that may need be modified for your environment are double commented (##). The rest of the script can safely be left as is.
+# Check for the latest version or report issues at https://github.com/sbirdsill/PowerShell-PC-Inventory
+#
+# IMPORTANT: 
+# * Parts that may need be modified for your environment are double commented (##). The rest of the script can safely be left as is.
+# * New versions of this script are not backwards-compatible with previous versions. 
+# * If you have upgraded to a new version of the script, please delete or archive the CSV files outputted by the previous versions before running it.
 
 ## CSV File Location (If the CSV file doesn't exist, the script will attempt to create it. Users will need full control of the file.)
 ## Also, make sure to create the InventoryOutput folder the CSV files will reside in. This script will not create folders automatically.
-$csv = "$pwd\InventoryOutput\$env:computername-Inventory.csv"
+$csv = "C:\Scripts\InventoryOutput\$env:computername-Inventory.csv"
 
 ## Error log path (Optional but recommended. If this doesn't exist, the script will attempt to create it. Users will need full control of the file.)
-$ErrorLogPath = "$pwd\PowerShell-PC-Inventory-Error-Log.log"
+$ErrorLogPath = "C:\Scripts\PowerShell-PC-Inventory-Error-Log.log"
+
+## Set the checkInstalledApps variable to 1 if you want the script to output the list of installed apps on the device.
+$checkInstalledApps = 0
 
 function ConcatenateInventory {
   # If the -c flag is specified, this function will consolidate the inventory files into a presentable report.
@@ -68,30 +77,56 @@ Write-Host "Gathering inventory information..."
 # Date
 $Date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-# IP
-$IP = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.DefaultIPGateway -ne $null }).IPAddress | Select-Object -First 1
+# Installed apps
+if ($checkInstalledApps -eq 1) {
+  # Get list of installed applications from registry
+  $installedApps = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+     -ErrorAction SilentlyContinue |
+  Select-Object -ExpandProperty DisplayName
 
-# MAC address
-$MAC = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.DefaultIPGateway -ne $null } | Select-Object -ExpandProperty MACAddress
+  # Filter out empty entries and join them into a comma-separated list
+  $appsList = ($installedApps | Where-Object { $_ -ne $null }) -join "`n "
+
+  # Output the variable
+  $appsList
+} else {
+  $appsList = "N/A"
+}
+
+# BIOS Version/Date
+$BIOSManufacturer = Get-CimInstance -Class win32_bios | Select-Object -ExpandProperty Manufacturer
+$BIOSVersion = Get-CimInstance -Class win32_bios | Select-Object -ExpandProperty SMBIOSBIOSVersion
+$BIOSName = Get-CimInstance -Class win32_bios | Select-Object -ExpandProperty Name
+$BIOS = Write-Output $BIOSManufacturer", "$BIOSVersion", "$BIOSName
+
+# IP and MAC address
+# Get the default route with the lowest metric
+$defaultRoute = Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Sort-Object -Property Metric | Select-Object -First 1
+# Get the interface index for the default route
+$interfaceIndex = $defaultRoute.InterfaceIndex
+# Get the IP address associated with this interface
+$interfaceIP = (Get-NetIPAddress -InterfaceIndex $interfaceIndex | Where-Object { $_.AddressFamily -eq "IPv4" }).IPAddress
+# Get the MAC address associated with this interface
+$interfaceMAC = (Get-NetAdapter -InterfaceIndex $interfaceIndex).MacAddress
 
 # Serial Number
-$SN = Get-WmiObject -Class Win32_Bios | Select-Object -ExpandProperty SerialNumber
+$SN = Get-CimInstance -Class Win32_Bios | Select-Object -ExpandProperty SerialNumber
 
 # Model
-$Model = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model
+$Model = Get-CimInstance -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model
 
 # CPU
-$CPU = Get-WmiObject -Class win32_processor | Select-Object -ExpandProperty Name
+$CPU = Get-CimInstance -Class win32_processor | Select-Object -ExpandProperty Name
 
 # RAM
-$RAM = Get-WmiObject -Class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object { [math]::Round(($_.sum / 1GB),2) }
+$RAM = Get-CimInstance -Class Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object { [math]::Round(($_.sum / 1GB),2) }
 
 # Storage
-$Storage = Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='$env:systemdrive'" | ForEach-Object { [math]::Round($_.Size / 1GB,2) }
+$Storage = Get-CimInstance -Class Win32_LogicalDisk -Filter "DeviceID='$env:systemdrive'" | ForEach-Object { [math]::Round($_.Size / 1GB,2) }
 
 #GPU(s)
 function GetGPUInfo {
-  $GPUs = Get-WmiObject -Class Win32_VideoController
+  $GPUs = Get-CimInstance -Class Win32_VideoController
   foreach ($GPU in $GPUs) {
     $GPU | Select-Object -ExpandProperty Description
   }
@@ -102,10 +137,18 @@ $GPU0 = GetGPUInfo | Select-Object -Index 0
 $GPU1 = GetGPUInfo | Select-Object -Index 1
 
 # OS
-$OS = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty Caption
+$OS = Get-CimInstance -Class Win32_OperatingSystem
 
 # OS Build
 $OSBuild = (Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')
+
+# Up time
+# Get the last boot time of the system
+$lastBootTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
+# Calculate the uptime by subtracting the last boot time from the current time
+$uptime = (Get-Date) - $lastBootTime
+# Output the uptime in a readable format
+$uptimeReadable = "{0} days, {1} hours, {2} minutes" -f $uptime.Days,$uptime.Hours,$uptime.Minutes
 
 # Username
 $Username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
@@ -113,7 +156,7 @@ $Username = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 # Monitor(s)
 function GetMonitorInfo {
   # Thanks to https://github.com/MaxAnderson95/Get-Monitor-Information
-  $Monitors = Get-WmiObject -Namespace "root\WMI" -Class "WMIMonitorID"
+  $Monitors = Get-CimInstance -Namespace "root\WMI" -Class "WMIMonitorID"
   foreach ($Monitor in $Monitors) {
     ([System.Text.Encoding]::ASCII.GetString($Monitor.ManufacturerName)).Replace("$([char]0x0000)","")
     ([System.Text.Encoding]::ASCII.GetString($Monitor.UserFriendlyName)).Replace("$([char]0x0000)","")
@@ -168,8 +211,6 @@ $ChassisDescription = switch ($Chassis) {
   default { "Unknown" }
 }
 
-$ChassisDescription
-
 # Function to write the inventory to the CSV file
 function OutputToCSV {
   # CSV properties
@@ -177,35 +218,36 @@ function OutputToCSV {
   Write-Host "Adding inventory information to the CSV file..."
   $infoObject = New-Object PSObject
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Date Collected" -Value $Date
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "IP Address" -Value $IP
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Hostname" -Value $env:computername
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "MAC Address" -Value $MAC
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "IP Address" -Value $interfaceIP
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "MAC Address" -Value $interfaceMAC
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "User" -Value $Username
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Type" -Value $ChassisDescription
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Serial Number/Service Tag" -Value $SN
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Model" -Value $Model
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "BIOS" -Value $BIOS
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "CPU" -Value $CPU
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "RAM (GB)" -Value $RAM
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Storage (GB)" -Value $Storage
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "GPU 0" -Value $GPU0
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "GPU 1" -Value $GPU1
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS" -Value $OS
-  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS Version" -Value $OSBuild
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS" -Value $os.Caption
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "OS Version" -Value $os.BuildNumber
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Up time" -Value $uptimeReadable
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 1" -Value $Monitor1
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 1 Serial Number" -Value $Monitor1SN
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 2" -Value $Monitor2
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 2 Serial Number" -Value $Monitor2SN
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 3" -Value $Monitor3
   Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Monitor 3 Serial Number" -Value $Monitor3SN
+  Add-Member -InputObject $infoObject -MemberType NoteProperty -Name "Installed Apps" -Value $appsList
   $infoObject
   $infoColl += $infoObject
 
   # Output to CSV file
   try {
-    $infoColl | Export-Csv -Path $csv -NoTypeInformation -Append
+    $infoColl | Export-Csv -Path $csv -NoTypeInformation
     Write-Host -ForegroundColor Green "Inventory was successfully updated!"
-    # Clean up empty rows
-    (Get-Content $csv) -notlike ",,,,,,,,,,,,,,,,,,,,*" | Set-Content $csv
     exit 0
   }
   catch {
